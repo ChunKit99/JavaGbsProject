571359a92542224f329ce984d288a9cb3fd28ff0
LIEW CHUN KIT <61322992+ChunKit99@users.noreply.github.com>
2021-04-20 11:03:42 +0800

    create update testRunGui fo testing

--- src/gui/TestRunGuiCode.java
+++ src/gui/TestRunGuiCode.java
@@ -6,13 +6,16 @@
 package gui;
 
 /**
+ * you can copy this code to other file for testing gui, or direct edit but
+ * after edit when commit do not include this file
  *
  * @author Liew Chun Kit
  */
 public class TestRunGuiCode {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Login loginFrame = new Login();
         loginFrame.setVisible(true);
-        //
+        //add your code to modify
     }
 }
